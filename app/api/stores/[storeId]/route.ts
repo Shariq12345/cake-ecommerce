95d6e38fbc0c023202779aa7e87860b6bd288559
Shariq Shaikh <shariqshaikh11042004@gmail.com>
2024-07-08 23:51:48 +0530

    Added Settings Route

---
+++ app/api/stores/[storeId]/route.ts
@@ -0,0 +1,72 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import db from "@/lib/db";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store ID is required", { status: 400 });
+    }
+
+    const store = await db.store.updateMany({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+      data: {
+        name,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORE_PATCH]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  // REQUEST IS NOT BUT WE CANNOT ONLY USE THE PARAMS
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store ID is required", { status: 400 });
+    }
+
+    const store = await db.store.deleteMany({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORE_DELETE]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
---
+++ components/alert-modal.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import Modal from "./modal";
+import { Button } from "./ui/button";
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  loading: boolean;
+};
+
+const AlertModal = ({ isOpen, onClose, onConfirm, loading }: Props) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <Modal
+      title="Are you sure"
+      description="You are about to delete this store. This action cannot be undone."
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className="pt-6 space-x-2 items-center justify-end w-full">
+        <Button variant="outline" disabled={loading} onClick={onClose}>
+          Cancel
+        </Button>
+        <Button disabled={loading} onClick={onConfirm} variant={"destructive"}>
+          Confirm
+        </Button>
+      </div>
+    </Modal>
+  );
+};
+
+export default AlertModal;
